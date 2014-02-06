047e07416269f98a1be5760355d0234c85ed6142
Christopher V. Ingebrigtsen <alkalin3@gmail.com>
2014-02-06 14:32:24 -0600

    required haml

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -8,6 +8,7 @@ require 'surveyor/acts_as_response'
 # require 'surveyor/surveyor_controller_methods'
 # require 'surveyor/models/survey_methods'
 require 'formtastic'
+require 'haml'
 Formtastic::FormBuilder.default_text_area_height = 5
 Formtastic::FormBuilder.default_text_area_width = 50
 Formtastic::FormBuilder.all_fields_required_by_default = false
